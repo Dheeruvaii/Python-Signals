9af1c0131545834093d5bb78e397d745ef7e0ac7
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-03-04 13:15:45 +0545

    update signals functions

---
+++ SignalsProject/signals_app/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Stock,Purchase
+
+@receiver(post_save,sender=Purchase)
+def update_stock(sender,instance,**kwargs):
+    stock=Stock.objects.get(available_item=instance.name)
+    stock.available_quantity=stock.available_quantity -instance.quantity
+    stock.save()
