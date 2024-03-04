e2ae4477f8ed1043c51f5d795fee6a5d1ce7ba4c
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-03-04 13:28:34 +0545

    updated views with viewsets

--- SignalsProject/signals_app/views.py
+++ SignalsProject/signals_app/views.py
@@ -1,1 +1,12 @@
 from .models import *
+from .serializer import *
+from rest_framework import viewsets
+
+class PurchaseViewset(viewsets.ModelViewSet):
+    queryset=Purchase.objects.all()
+    serializer_class=PurchaseSerializer
+
+
+class StockViewset(viewsets.ModelViewSet):
+    queryset=Stock.objects.all()
+    serializer_class=StockSerializer
