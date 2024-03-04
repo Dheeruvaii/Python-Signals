c40e8fd593da8792dd1d9d7f9c73ae4014cc151d
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-03-04 13:44:30 +0545

    Register Purchase and Stock models with admin site

--- SignalsProject/signals_app/admin.py
+++ SignalsProject/signals_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Stock)
-admin.site.register(Purchase)
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ('item', 'quantity')
 
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('available_item', 'available_quantity')
